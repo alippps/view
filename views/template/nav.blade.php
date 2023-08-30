2a0926d1d27654f670e7ee03967f0a20d486ffed
alippps <91925776+alippps@users.noreply.github.com>
2023-08-30 16:15:09 +0700

    Add files via upload
    pake aja dik

---
+++ views/template/nav.blade.php
@@ -0,0 +1,8 @@
+<li class="nav-item @if ($active == 'Spp') {
+    {{ 'active' }}
+} @endif">
+    <a class="nav-link"href="{{url('spp') }}">
+        <i class="fas fa-fw fa-cube"></i>
+        <span>Spp</span>
+    </a>
+</li>
